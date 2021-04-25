8403fe7ec1c06954d818766d97e2fb9d0c415ac4
Bui Duc Quan <gcssire01@gmail.com>
2021-04-25 17:50:44 +0700

    Delete lib/quan directory

--- lib/quan/quan_event.dart
+++
@@ -1,9 +0,0 @@
-abstract class QuanEvent {
-  num increment;
-}
-
-class IncreaseNumber extends QuanEvent {
-  //Event to help user increase number
-  IncreaseNumber(this.increment);
-  final int increment;
-}
--- lib/quan/quan_state.dart
+++
@@ -1,4 +0,0 @@
-class QuanState {
-  QuanState(this.number);
-  final int number;
-}
